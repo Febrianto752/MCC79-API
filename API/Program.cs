bfb1d8923ad230b27b9059a2acc43a419a3e18d5
Febrianto <febrianto.bekasi@gmail.com>
2023-06-23 13:04:23 +0700

    feat: add crud role api

--- API/Program.cs
+++ API/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddScoped<IEducationRepository, EducationRepository>();
 builder.Services.AddScoped<IRoomRepository, RoomRepository>();
 builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
