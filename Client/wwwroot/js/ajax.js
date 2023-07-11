0df2b1e2aef54dcaff82d225e98d350099bcd344
Febrianto752 <febrianto.bekasi@gmail.com>
2023-07-11 20:47:41 +0700

    feat: add delete feature and sweet alert

--- Client/wwwroot/js/ajax.js
+++ Client/wwwroot/js/ajax.js
@@ -1,27 +1,12 @@
-const POKEMON_API_URL = "https://pokeapi.co/api/v2/pokemon";
 const EMPLOYEE_API_URL = "https://localhost:7103/api/v1/employees";
-//$.ajax({
-//    url: POKEMON_API_URL,
-//}).done((data) => {
-//    let pokemonRow = "";
-//    $.each(data.results, (key, val) => {
-//        pokemonRow += `<tr>
-//                  <td>${key + 1}</td>
-//                  <td>${val.name}</td>
-//                  <td><button onclick="detail('${val.url
-//            }')" data-bs-toggle="modal" data-bs-target="#modal-pokemon" class="btn btn-primary">Detail</button></td>
-//              </tr>`;
-//    });
-//    $("#tbody").html(pokemonRow);
-//});
 
 $(document).ready(function () {
-    let table = $('#employee-table').DataTable({
+    $('#employee-table').DataTable({
         
         ajax: {
             url: EMPLOYEE_API_URL,
             dataType: "JSON",
-            dataSrc: "data" //data source -> butuh array of object
+            dataSrc: "data"
         },
         columns: [
             {
@@ -38,12 +23,6 @@ $(document).ready(function () {
                 render: (data, type, row) => {
                     return `${row.firstName} ${row.lastName}`;
                 }
-                //render: function (data, type, row) {
-                //    console.log("data : ", data);
-                //    console.log("type : ", type);
-                //    console.log("row : ", row);
-                //    return `<button onclick="detail('${data}')" data-bs-toggle="modal" data-bs-target="#modal-pokemon" class="btn btn-primary">Detail</button>`;
-                //}
             },
             {
                 data: "gender",
@@ -64,9 +43,9 @@ $(document).ready(function () {
                 }
             },
             {
-                data: "detail",
+                data: "delete",
                 render: (data, type, row) => {
-                    return `<button class="btn btn-info">Detail</button>`
+                    return `<button class="btn btn-danger" onclick="Delete('${row.guid}')">Delete</button>`
                 }
             }
         ],
@@ -110,11 +89,8 @@ $(document).ready(function () {
     });
 
     $("#btnCreateEmployee").on("click", () => {
-        Insert();
-        
-    });
-
-    
+        Insert();       
+    }); 
 });
 
 function Insert() {
@@ -141,53 +117,23 @@ function Insert() {
         .done((result) => {
             console.log("result : ", result);
             //buat alert pemberitahuan jika success
-            const successMessageElem = `
-      <div
-          class="alert alert-success alert-dismissible fade alertMessage"
-          role="alert"
-      >
-        <div class="text-center">
-          ${result.message}       
-        </div>
+            Swal.fire({
+                title: result.message,
+                icon: 'success',
+                confirmButtonText: 'OK'
+            })
 
-        <button
-          type="button"
-          class="btn-close"
-          data-bs-dismiss="alert"
-          aria-label="Close"
-        ></button>
-      </div>      
-      `;
-
-            $("#flashMessage").html(successMessageElem);
-            $(".alertMessage").addClass("show");
+            // reload table 
             $('#employee-table').DataTable().ajax.reload();
         })
         .fail((error) => {
             console.log("Error : ", error);
-            const errorMessages = error.responseJSON.errors;
 
-            const listErrorElem = `
-      <div
-          class="alert alert-danger alert-dismissible fade alertMessage"
-          role="alert"
-      >
-        <strong>Errors Message : </strong>
-        <ul>
-        ${errorMessages.map((errorMessage) => `<li>${errorMessage}</li>`)}
-        </ul>
-
-        <button
-          type="button"
-          class="btn-close"
-          data-bs-dismiss="alert"
-          aria-label="Close"
-        ></button>
-      </div>      
-      `;
-
-            $("#flashMessage").html(listErrorElem);
-            $(".alertMessage").addClass("show");
+            Swal.fire({
+                title: error.responseJSON.message,
+                icon: 'error',
+                confirmButtonText: 'OK'
+            })
         })
         .always(() => {
             $("#firstName").val("");
@@ -200,121 +146,40 @@ function Insert() {
         });
 }
 
-//function detail(stringURL) {
-//    loadingImgAnimation();
-//    loadingDescriptionAnimation();
+function Delete(guid) {
 
-//    $("#nav-description-tab").trigger("click");
-//    $.ajax({
-//        url: stringURL,
-//    }).done((res) => {
-//        $(".modal-title").html(res.name.toUpperCase());
+    const agreeDeleteData = confirm("are you sure ? ");
 
-//        $("#pokemon-image").attr(
-//            "src",
-//            res.sprites.other["official-artwork"]["front_default"]
-//        );
+    if (agreeDeleteData) {
+        $.ajax({
+            url: EMPLOYEE_API_URL + `?guid=${guid}`,
+            type: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+        .done((result) => {
+            console.log("result : ", result);
+            //buat alert pemberitahuan jika success
+            Swal.fire({
+                title: result.message,
+                icon: 'success',
+                confirmButtonText: 'OK'
+            })
 
-//        let pokemonTypes = "";
+            // reload table 
+            $('#employee-table').DataTable().ajax.reload();
+        })
+        .fail((error) => {
+            console.log("Error : ", error);
 
-//        res.types.forEach((type) => {
-//            pokemonTypes += `
-//      <span class="badge rounded-pill bg-dark">${type.type.name}</span>
-//      `;
-//        });
+            Swal.fire({
+                title: error.responseJSON.message,
+                icon: 'error',
+                confirmButtonText: 'OK'
+            })
+        });
+    }
+    
+}
 
-//        $("#pokemon-type").html(pokemonTypes);
-
-//        setDescriptionPokemon(res.species.url);
-//        $("#height-description").html(`<b>Height</b> : ${res.height} decimetres`);
-//        $("#weight-description").html(`<b>Weight</b> : ${res.weight} hectograms`);
-
-//        // set abilities tab
-//        let abilitieElems = "";
-
-//        res.abilities.forEach((ability) => {
-//            abilitieElems += `<li>${ability.ability.name}</li>`;
-//        });
-
-//        $("#ability-list").html(abilitieElems);
-
-//        // set stats tab
-//        $("#progress-hp").attr("style", `width: ${res.stats[0].base_stat}%`);
-//        $("#progress-hp").html(`HP : ${res.stats[0].base_stat}`);
-
-//        $("#progress-attack").attr("style", `width: ${res.stats[1].base_stat}%`);
-//        $("#progress-attack").html(`ATTACK : ${res.stats[1].base_stat}`);
-
-//        $("#progress-defense").attr("style", `width: ${res.stats[2].base_stat}%`);
-//        $("#progress-defense").html(`DEFENSE : ${res.stats[2].base_stat}`);
-
-//        $("#progress-speed").attr("style", `width: ${res.stats[3].base_stat}%`);
-//        $("#progress-speed").html(`SPEED : ${res.stats[3].base_stat}`);
-
-//        //removeLoadingAnimation();
-//    });
-//}
-
-//$("#pokemon-image").on("load", function () {
-//    console.log("Hello World");
-//    removeLoadingImgAnimation();
-//});
-
-//function setDescriptionPokemon(stringUrl) {
-//    $.ajax({
-//        url: stringUrl,
-//    }).done((res) => {
-//        let pokemonDescription = "";
-
-//        let flavorTextEntriesEN = res.flavor_text_entries.filter(
-//            (entrie) => entrie.language.name == "en"
-//        );
-
-//        let uniqueFlavorTexts = Array.from(
-//            new Set(
-//                flavorTextEntriesEN.map((item) =>
-//                    item.flavor_text
-//                        .replace("\f", " ")
-//                        .replace(/(\r\n|\n|\r)/gm, " ")
-//                        .toLowerCase()
-//                )
-//            )
-//        );
-
-//        uniqueFlavorTexts.forEach((flavor_text) => {
-//            pokemonDescription += `
-//        <p>
-//          ${flavor_text}
-//        </p>
-//      `;
-//        });
-
-//        $(".p-description").html(pokemonDescription);
-//        removeLoadingDescriptionAnimation();
-//    });
-//}
-
-//const loadingElem = `
-//<div class="loading position-absolute bg-white h-100 d-flex justify-content-center align-items-center" style="top:0; width: 95%">
-//  <div class="spinner-border " role="status">
-//    <span class="visually-hidden">Loading...</span>
-//  </div>
-//</div>
-
-//`;
-
-//function loadingImgAnimation() {
-//    $("#left-side-modal-pokemon").append(loadingElem);
-//}
-
-//function loadingDescriptionAnimation() {
-//     $("#right-side-modal-pokemon").append(loadingElem);
-//}
-
-//function removeLoadingImgAnimation() {
-//    $("#left-side-modal-pokemon .loading").remove();
-//}
-
-//function removeLoadingDescriptionAnimation() {
-//    $("#right-side-modal-pokemon .loading").remove();
-//}
