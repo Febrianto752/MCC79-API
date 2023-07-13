a85d0f2cb8606dc0ef88f50efe002c42a719f3c0
Febrianto752 <febrianto.bekasi@gmail.com>
2023-07-13 21:43:32 +0700

    fix: change architecture client

--- Client/Controllers/EmployeeController.cs
+++ Client/Controllers/EmployeeController.cs
@@ -1,12 +1,118 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.DTOs.Employees;
+using Client.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Client.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        private readonly IEmployeeRepository repository;
+
+        public EmployeeController(IEmployeeRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await repository.Get();
+            var ListEmployee = new List<GetEmployeeDto>();
+
+            if (result.Data != null)
+            {
+                ListEmployee = result.Data.ToList();
+            }
+            return View(ListEmployee);
+        }
+
+        public async Task<IActionResult> Details(Guid guid)
+        {
+            var result = await repository.Get(guid);
+            var employee = new GetEmployeeDto();
+
+            if (result.Data != null)
+            {
+                employee = result.Data;
+            }
+            return View(employee);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(GetEmployeeDto newEmploye)
+        {
+
+            var result = await repository.Post(newEmploye);
+            if (result.Code == 201)
+            {
+                TempData["Success"] = "Data berhasil masuk";
+                return RedirectToAction(nameof(Index));
+            }
+            else if (result.Code == 409)
+            {
+                ModelState.AddModelError(string.Empty, result.Message);
+                return View();
+            }
+
+            TempData["Error"] = result.Message;
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid guid)
+        {
+            var result = await repository.Get(guid);
+            var employee = new GetEmployeeDto();
+
+            if (result.Data != null)
+            {
+                employee = result.Data;
+            }
+            return View(employee);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(GetEmployeeDto updateEmployee)
+        {
+
+            var result = await repository.Put(updateEmployee);
+            if (result.Code == 200)
+            {
+                TempData["Success"] = result.Message;
+                return RedirectToAction(nameof(Index));
+            }
+
+            TempData["Error"] = result.Message;
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePOST(Guid guid)
+        {
+            Console.WriteLine("guid : " + guid);
+            var result = await repository.Delete(guid);
+            if (result.Code == 200)
+            {
+                TempData["Success"] = "Data berhasil dihapus";
+                return RedirectToAction(nameof(Index));
+            }
+            else if (result.Code == 404 || result.Code == 500)
+            {
+
+                TempData["Success"] = result.Message;
+                return RedirectToAction(nameof(Index));
+            }
+
+
+            return RedirectToAction(nameof(Index));
         }
 
     }
---
+++ Client/Repositories/GeneralRepository.cs
@@ -0,0 +1,81 @@
+﻿using API.Utilities.Handlers;
+using Client.Contracts;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Client.Repositories
+{
+    public class GeneralRepository<Entity, TId> : IRepository<Entity, TId>
+        where Entity : class
+    {
+        private readonly string request;
+        private readonly HttpClient httpClient;
+
+        public GeneralRepository(string request)
+        {
+            //this.request = request;
+            httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7103/api/v1/")
+            };
+            this.request = request;
+        }
+
+        public async Task<ResponseHandler<Entity>> Delete(TId id)
+        {
+            ResponseHandler<Entity> entityVM = null;
+            using (var response = await httpClient.DeleteAsync($"{request}?guid={id}"))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                entityVM = JsonConvert.DeserializeObject<ResponseHandler<Entity>>(apiResponse);
+            }
+            return entityVM;
+        }
+
+        public async Task<ResponseHandler<IEnumerable<Entity>>> Get()
+        {
+            ResponseHandler<IEnumerable<Entity>> entityVM = null;
+            using (var response = await httpClient.GetAsync(request))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                entityVM = JsonConvert.DeserializeObject<ResponseHandler<IEnumerable<Entity>>>(apiResponse);
+            }
+            return entityVM;
+        }
+
+        public async Task<ResponseHandler<Entity>> Get(TId id)
+        {
+            ResponseHandler<Entity> entityVM = null;
+            using (var response = await httpClient.GetAsync($"{request}{id}/"))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                entityVM = JsonConvert.DeserializeObject<ResponseHandler<Entity>>(apiResponse);
+            }
+            return entityVM;
+        }
+
+        public async Task<ResponseHandler<Entity>> Post(Entity entity)
+        {
+            ResponseHandler<Entity> entityVM = null;
+            StringContent content = new StringContent(JsonConvert.SerializeObject(entity), Encoding.UTF8, "application/json");
+            using (var response = httpClient.PostAsync(request, content).Result)
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                entityVM = JsonConvert.DeserializeObject<ResponseHandler<Entity>>(apiResponse);
+            }
+            return entityVM;
+        }
+
+        public async Task<ResponseHandler<Entity>> Put(Entity entity)
+        {
+            ResponseHandler<Entity> entityVM = null;
+            StringContent content = new StringContent(JsonConvert.SerializeObject(entity), Encoding.UTF8, "application/json");
+            using (var response = httpClient.PutAsync(request, content).Result)
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                entityVM = JsonConvert.DeserializeObject<ResponseHandler<Entity>>(apiResponse);
+            }
+            return entityVM;
+        }
+    }
+}
